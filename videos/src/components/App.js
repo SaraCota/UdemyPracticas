e3af6bffe595c0bd41e32e1a5f3909ec38c894bd
SaraCota <sarigna.art@gmail.com>
2022-01-10 12:15:38 -0700

    Avance app videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount(){
+    this.onTermSubmit('puppies');
+  }
 
   onTermSubmit = async (searchterm) => {
     //SOLICITUD DE API ASÍNCRONA
@@ -13,14 +19,23 @@ class App extends React.Component {
         q: searchterm,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
   };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video});
+  }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>I have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column"><VideoDetail video={this.state.selectedVideo}/></div>
+            <div className="five wide column"><VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/></div>
+          </div>
+        </div>
       </div>
     );
   }
